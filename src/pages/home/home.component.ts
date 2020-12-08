7e74b21eecd346d4c4708bf3a0c19f099a6e17d1
sjn <shijunnan8688@dingtalk.com>
2020-12-08 19:47:00 +0800

    feat: 测试格式化

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  public ngOnInit() {
+    console.log(1, 2, 3, 4, 5, 6, 7, 8);
+    const a = { a: 1, b: 2, foo: 3 };
+  }
+}
